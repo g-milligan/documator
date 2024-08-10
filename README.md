94b4393cdd30f0965ce0d7ba4d212e66741f9d1f
Gregg Milligan II <milligangregg@gmail.com>
2024-08-10 16:20:20 -0400

    reset java project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Documator
 
+[Project Plan](https://docs.google.com/document/d/1mz5JSz6ZhyKSAv1Pkq7J7JFwwLZ-jJVO-OhUM67w0e0/edit?usp=sharing)
--- src/java/src/main/java/gii/example/backend/Application.java
+++ src/java/src/main/java/gii/example/backend/Application.java
@@ -1,4 +1,4 @@
-package com.gii.documator;
+package gii.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
