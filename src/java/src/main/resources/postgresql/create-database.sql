311eb53897f9384642586fe3d03cacafe3db29f5
Gregg Milligan II <milligangregg@gmail.com>
2024-08-11 23:18:35 -0400

    begin making the user database scripts

--- src/java/src/main/resources/postgresql/create-database.sql
+++ src/java/src/main/resources/postgresql/create-database.sql
@@ -18,3 +18,18 @@ CREATE TABLE IF NOT EXISTS task (
     task_modified        timestamp not null default now()
 );
 
+CREATE TYPE UserGroupEnum AS ENUM (
+    'VIEWER', 
+    'EDITOR', 
+    'ADMIN');
+
+CREATE TABLE IF NOT EXISTS user (
+    user_id              serial primary key,
+    user_name            varchar(510),
+    user_email           varchar(510) not null,
+    user_secret          varchar(510) not null,
+    user_active          boolean not null default true,
+    user_group           UserGroupEnum not null default 'VIEWER',
+    user_created         timestamp not null default now(),
+    user_modified        timestamp not null default now()
+);
--- src/java/src/main/resources/postgresql/make-sample-data.sql
+++ src/java/src/main/resources/postgresql/make-sample-data.sql
@@ -2,18 +2,16 @@ INSERT INTO task(
     task_summary
 )
 VALUES 
-(
-    'Wake Up'
-),
-(
-    'Fold Clothes'
-),
-(
-    'Apply to Jobs'
-),
-(
-    'Practice Programming'
-),
-(
-    'All Fly'
-);
+( 'Wake Up' ),
+( 'Fold Clothes' ),
+( 'Apply to Jobs' ),
+( 'Practice Programming' ),
+( 'All Fly' );
+
+INSERT INTO user(
+    user_name, user_email, user_secret, user_group
+)
+VALUES 
+( 'My Admin', 'milligangregg@gmail.com', '1234', 'ADMIN' ),
+( 'My Editor', 'milligangregg@gmail.com', '123', 'EDITOR' ),
+( 'My Viewer', 'tyleralias@hotmail.com', '12', 'VIEWER' );
